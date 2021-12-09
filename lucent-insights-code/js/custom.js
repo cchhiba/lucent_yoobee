978cfa0dd2a652fdd6d0e1839629961b07b5ef4c
Charissa Chhiba <270036318@bun2-206-02.acg.ac.nz>
2021-12-10 11:53:37 +1300

    first commit

---
+++ lucent-insights-code/js/custom.js
@@ -0,0 +1,10 @@
+/* this is the custom js file for lucent design iphone X and ipad*/
+
+$(document).ready(() => {
+  $('.cards').slick({
+    centerMode: true,
+    centerPadding: '50px',
+    slidesToShow: 1,
+    dots: true
+  });
+});
